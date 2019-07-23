ebd7e70a6accd64b64b86db8be911af528b89f05
sjh <17600697069@163.com>
2019-07-23 17:11:35 +0800

    init

---
+++ zk-api/src/main/java/com/example/zkapi/ZkApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.zkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkApiApplication.class, args);
+	}
+
+}
---
+++ zk-api/src/test/java/com/example/zkapi/ZkApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.zkapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZkApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
