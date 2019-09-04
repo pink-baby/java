4daa5a54ef960f06e2ef9c833dad898ab5ae98c7
sjh <17600697069@163.com>
2019-09-04 11:43:50 +0800

    hbase api2

---
+++ hbase-api/src/main/java/com/example/hbaseapi/HbaseApiApplication.java
@@ -0,0 +1,13 @@
+//package com.example.hbaseapi;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class HbaseApiApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(HbaseApiApplication.class, args);
+//	}
+//
+//}
