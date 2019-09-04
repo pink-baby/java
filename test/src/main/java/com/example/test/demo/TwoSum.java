67197f6f47bdb75c31e1b1a28bb6e8925f5fabe5
sjh <17600697069@163.com>
2019-09-04 11:43:12 +0800

    hbase api

---
+++ test/src/main/java/com/example/test/demo/TwoSum.java
@@ -0,0 +1,88 @@
+package com.example.test.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+//        int[] arr = {1,2, 7, 11, 15};
+//        int target = 9;
+//        int[] result = twoSum2(arr, target);
+//        for (int i : result) {
+//            System.out.println(i);
+//        }
+
+        String roman = "MIII";
+        System.out.println(romanToInt(roman));
+    }
+
+    public static int[] twoSumForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    /**
+     * 面试题罗马数字装int类型
+     * @param 罗马数字表示
+     * @return
+     */
+    public static int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        int ans = 0;
+        for (int i = 0; i < s.length(); ) {
+            if (i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                ans += map.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                ans += map.get(s.substring(i, i + 1));
+                i++;
+            }
+        }
+        return ans;
+    }
+
+}
